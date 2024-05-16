c03f8bde985d82fdae795b0b48539b647bcb9833
SkorpionHG <125448768+SkorpionHG@users.noreply.github.com>
2024-05-16 18:28:06 +0700

    first js task done, trying to do some php

--- Javascript/football.js
+++ Javascript/football.js
@@ -1,6 +1,37 @@
 function GenerateNumLine() {
-    let lineLength = Math.floor(Math.random() * 100) + 7;
-    console.log(lineLength)
+    let lineLength = Math.floor(Math.random() * 94) + 7;
+    let line = "";
+
+    for (let i = 0; i < lineLength; i++) {
+        line += Math.round(Math.random()).toString();
+    }
+
+    return line;
 }
 
-GenerateNumLine();
+function CheckIfSituationIsDangerous(line) {
+    let count = 0;
+    let dangerous = false;
+    for (let i = 0; i < line.length; i++) {
+        switch (line[i]) {
+            case "1":
+                count++;
+                break;
+            case "0":
+                count = 0;
+                break;
+        }
+        if(count == 7) {
+            dangerous = true;
+            break;
+        }
+    }
+    if(dangerous) {
+        return "YES";
+    }
+    return "NO";
+}
+
+let line = GenerateNumLine();
+console.log(line);
+console.log(CheckIfSituationIsDangerous(line));
